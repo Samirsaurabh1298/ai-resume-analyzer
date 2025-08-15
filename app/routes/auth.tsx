fb22de63576ea69898be169d20d67173d85d1e73
SAURABH SAMIR - MFCWL <23269636@mahindra.com>
2025-08-15 21:01:24 +0530

    Resume feedback page

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,9 +1,9 @@
+import { usePuterStore } from "~/lib/puter";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router";
-import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => [
-  { title: "Cvmind | Auth" },
+  { title: "Resumind | Auth" },
   { name: "description", content: "Log into your account" },
 ];
 
@@ -18,27 +18,27 @@ const Auth = () => {
   }, [auth.isAuthenticated, next]);
 
   return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
       <div className="gradient-border shadow-lg">
         <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
-          <div className="flex flex-col item-center gap-2 text-center">
+          <div className="flex flex-col items-center gap-2 text-center">
             <h1>Welcome</h1>
-            <h2>Please log in to your account</h2>
+            <h2>Log In to Continue Your Job Journey</h2>
           </div>
           <div>
             {isLoading ? (
               <button className="auth-button animate-pulse">
-                <p>Signing you in..</p>
+                <p>Signing you in...</p>
               </button>
             ) : (
               <>
                 {auth.isAuthenticated ? (
                   <button className="auth-button" onClick={auth.signOut}>
-                    <p>Log Out </p>
+                    <p>Log Out</p>
                   </button>
                 ) : (
                   <button className="auth-button" onClick={auth.signIn}>
-                    <p>Log in </p>
+                    <p>Log In</p>
                   </button>
                 )}
               </>
